7c3e3c69e2ce5261bfca0ccd3e4590c5ee88014e
Julien Sanmartin <julien.sanmartin@parsable.com>
2020-06-04 20:02:50 -0700

    refactor app to redirect more elegantly

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,26 +2,49 @@ import React, { useState, useEffect } from 'react';
 import Layout from '../components/Layout';
 import Router from 'next/router';
 import AuthenticatedContext from '../utils/AuthenticationContext';
+import { useLogoutSync } from '../utils/useLogoutSync';
+
+const nonProtectedRoutes = ['Signup', 'Login'];
+
+const needToRedirectedTo = (
+  isAuthenticated: boolean,
+  componentName: string
+) => {
+  const isProtectedRouteNotAuthenticated: boolean =
+    !isAuthenticated && !nonProtectedRoutes.includes(componentName);
+  const isNonProtectedRouteAlreadyAuthenticated: boolean =
+    isAuthenticated && nonProtectedRoutes.includes(componentName);
+  if (isProtectedRouteNotAuthenticated) {
+    return '/login';
+  } else if (isNonProtectedRouteAlreadyAuthenticated) {
+    return '/';
+  } else {
+    return undefined;
+  }
+};
 
 function MyApp({ Component, pageProps }) {
+  useLogoutSync();
   const [isAuthenticated, _setAuthenticated] = useState(false);
 
   useEffect(() => {
     const componentName = Component.prototype.constructor.name;
-    if (!isAuthenticated && componentName !== 'Signup') {
-      Router.push('/login');
-    } else if (
-      isAuthenticated &&
-      (componentName === 'Signup' || componentName === 'Login')
-    ) {
-      Router.push('/');
+    const redirectTo = needToRedirectedTo(isAuthenticated, componentName);
+    if (redirectTo) {
+      Router.push(redirectTo);
     }
   }, [isAuthenticated, Component]);
+
+  const componentName = Component.prototype.constructor.name;
 
   return (
     <AuthenticatedContext.Provider value={[isAuthenticated, _setAuthenticated]}>
       <Layout>
-        <Component {...pageProps} />
+        {needToRedirectedTo(isAuthenticated, componentName) ? (
+          'LOADING...'
+        ) : (
+          <Component {...pageProps} />
+        )}
       </Layout>
     </AuthenticatedContext.Provider>
   );
