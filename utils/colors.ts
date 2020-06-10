b8d08c4913889653d4577331f723430329e4439c
Julien Sanmartin <julien.sanmartin@parsable.com>
2020-06-09 19:47:14 -0700

    play with styles 2

---
+++ utils/colors.ts
@@ -0,0 +1,24 @@
+export const claret = '#751A33';
+export const mediumCarmine = '#B34233';
+export const chineseBronze = '#D28F33';
+export const vegasGold = '#D4B95E';
+export const cadetBlue = '#4EA2A2';
+export const metallicSeaweed = '#1A8693';
+
+export const bananaYellow = '#FFDB34';
+export const yellowOrange = '#FFAC46';
+export const sunsetOrange = '#F95D58';
+export const lavenderIndigo = '#9744E6';
+export const blueJean = '#5CBBFB';
+
+export const white = '#FFFFFF';
+export const black = '#000000';
+export const lightGrey = '#D3D3D3';
+export const extraLightGrey = '#EBECF0';
+
+export const primary = lavenderIndigo;
+export const secondary = mediumCarmine;
+export const tertiary = chineseBronze;
+
+export const mainBackgroundColor = extraLightGrey;
+export const headerFooterBackgroundColor = sunsetOrange;
